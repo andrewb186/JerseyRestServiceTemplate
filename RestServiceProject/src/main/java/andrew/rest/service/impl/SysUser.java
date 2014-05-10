662a6f226882b426b984cee287c033c10266141b
andrewb186 <andrewb186@gmail.com>
2014-05-10 19:14:07 +0200

    -Modified POM to set package (war) file filename as set in the properties section
    -Added SysUserResponse with XML Annotations
    
    -Modified SysUser.getUser method  to return a SysUserResponseObject

--- RestServiceProject/src/main/java/andrew/rest/service/impl/SysUser.java
+++ RestServiceProject/src/main/java/andrew/rest/service/impl/SysUser.java
@@ -2,11 +2,17 @@ package andrew.rest.service.impl;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 
 
+
+
+import javax.ws.rs.core.MediaType;
+
 import andrew.rest.service.interfaces.ISysUser;
+import andrew.rest.service.response.SysUserResponse;
 
 
 @Path("user")
@@ -17,11 +23,11 @@ public class SysUser implements ISysUser {
 	
 	
 	@GET
-	@Path("/getuser")
-	@Produces("text/plain")
-	public String getUser() {
+	@Path("/getuser/{ID}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public SysUserResponse getUser(@PathParam("ID") String id) {
 		log.error("Getting user with ID 10225360");
-		return "Andrew Brincat";
+		return new SysUserResponse();
 	}
 
 }
